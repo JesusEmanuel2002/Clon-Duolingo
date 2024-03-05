869110901aca6fb516e0003e2ac175a7c4bd469d
JesusEmanuel2002 <157553924+JesusEmanuel2002@users.noreply.github.com>
2024-03-05 14:22:23 -0600

    version final.js

---
+++ Vanilla.js
@@ -0,0 +1,142 @@
+const zona1 = document.getElementById("zona1");{
+    var gif=document.getElementById("daoulingo saltado.gif");
+    gif.src="daoulingo saltado" +".gif";
+    const h1 = document.createElement("h1");
+    h1.textContent = "¡La forma divertida, efectiva y gratis de aprender un idioma!";
+    const boton_1Button = document.createElement("boton_1");
+    addButton.type = "button";
+    addButton.textContent = "EMPIENZA AHORA";
+    const boton_2Button = document.createElement("boton_2");
+    addButton.type = "button";
+    addButton.textContent = "YA TENGO UNA CUENTA";
+}
+
+const zona2 = document.getElementById("zona2");{
+    var gif=document.getElementById("doulingo feliz.gif");
+    gif.src="doulingo feliz" +".gif";
+    const h1 = document.createElement("h1");
+    h1.textContent = "divertido, efectivo y gratis";
+    const p = document.createElement("p");
+    p.textContent = "Aprender con Duolingo es divertido y los estudios demuestran que funciona.¡En nuestras lecciones cortas ganarás puntos y habilitarás nuevas unidades al mismo tiempo que desarrollas tus habilidades de comunicación en la vida real!";
+}
+
+const zona3 = document.getElementById("zona3");{
+    var gif=document.getElementById("doulingo con campana.gif");
+    gif.src="doulingo con campana" +".gif";
+    const h1 = document.createElement("h1");
+    h1.textContent = "respaldado por la ciencia";
+    const p = document.createElement("p");
+    p.textContent = "Gracias a la combinación de métodos de enseñanza respaldados por la ciencia y un contenido entretenido, creamos cursos que enseñan de forma eficiente a leer, escribir, entender y hablar en otros idiomas.";
+}
+
+const zona4 = document.getElementById("zona4");{
+    var gif=document.getElementById("doulingo haciendo ejercicio.gif");
+    gif.src="doulingo haciendo ejercicio" +".gif";
+    const h1 = document.createElement("h1");
+    h1.textContent = "mantén tu motivación";
+    const p = document.createElement("p");
+    p.textContent = "Nuestras funcionalidades y desafíos son divertidos y hacen que aprender se sienta como un juego. ¡Vas a poder formar un hábito de aprendizaje sin esfuerzo! Y, por supuesto, recibirás recordatorios de nuestra adorable mascota, Duo, el búho.";
+}
+
+const zona6 = document.getElementById("zona6");{
+    var gif=document.getElementById("doulinho enojado.webp");
+    gif.src="doulinho enojado" +".webp";
+    const h3 = document.createElement("h3");
+    h3.textContent = "Poner up with";
+    const h1 = document.createElement("h1");
+    h1.textContent = "Super Doulingo";
+    const boton_3Button = document.createElement("boton_3");
+    addButton.type = "button";
+    addButton.textContent = "PRUEBA 2 SEMANAS GRATIS";
+}
+
+const zona7 = document.getElementById("zona7");{
+    var gif=document.getElementById("doulingo hablando ingles.gif");
+    gif.src="doulingo hablando ingles" +".gif";
+    const h1 = document.createElement("h1");
+    h1.textContent = "duolingo english test";
+    const p = document.createElement("p");
+    p.textContent = "Nuestro examen de inglés es conveniente, rápido y económico. El Duolingo English Test integra los últimos avances en la ciencia e inteligencia artificial para dar a todas las personas la posibilidad de elegir dónde y cuándo hacer el examen y que puedan dar lo mejor de sí.";
+    const boton_2Button = document.createElement("boton_2");
+    addButton.type = "button";
+    addButton.textContent = "CERTEFICA TU INGLES";
+}
+
+const zona8 = document.getElementById("zona8");{
+    var gif=document.getElementById("duolingo bailando.gif");
+    gif.src="duolingo bailando" +".gif";
+    const h1 = document.createElement("h1");
+    h1.textContent = "duolingo for schools";
+    const p = document.createElement("p");
+    p.textContent = "Maestras y maestros: ¡estamos para ayudarlos! Nuestra herramienta gratuita ayuda a tus estudiantes a aprender idiomas a través de la app de Duolingo, tanto dentro como fuera del salón de clases.";
+    const boton_2Button = document.createElement("boton_2");
+    addButton.type = "button";
+    addButton.textContent = "POTECNIA TU SALON DE CLASES";
+}
+
+const zona9 = document.getElementById("zona9");{
+    var gif=document.getElementById("Dpulingo musica.gif");
+    gif.src="Dpulingo musica" +".gif";
+    const h1 = document.createElement ("h1"); 
+    h1.textContent = "duolingo abc";
+    const p = document.createElement("p");
+    p.textContent = "¡Desde aprender idiomas hasta alfabetismo! Con lecciones de fonética y cuentos divertidos, Duolingo ABC enseña a niños y niñas de entre 3 y 8 años a leer y escribir… ¡y es totalmente gratis!";
+    const boton_2Button = document.createElement("boton_2");
+    addButton.type = "button";
+    addButton.textContent = "CONOCE DUOLINGO ABC";
+}
+
+const zona10 = document.getElementById("zona10");{
+    var gif=document.getElementById("duolingo llorando");
+    gif.src="duolingo llorando" +".gif";
+    const h1 = document.createElement ("h1"); 
+    h1.textContent = "duolingo math";
+    const p = document.createElement("p");
+    p.textContent = "¡Prueba nuestras lecciones cortas y gratis para matemáticas! Con Duolingo Math, los estudiantes podrán adelantarse en sus clases de matemáticas y los adultos podrán practicar para mejorar su destreza matemática.";
+    const boton_2Button = document.createElement("boton_2");
+    addButton.type = "button";
+    addButton.textContent = "MEJORA EN MATEMATICAS";
+}
+
+const zona11 = document.getElementById("zona11");{
+    const h1 = document.createElement ("h1"); 
+    h1.textContent = "aprende idiomas con duolingo";
+    const boton_1Button = document.createElement("boton_1");
+    addButton.type = "button";
+    addButton.textContent = "EMPIENZA AHORA";
+}
+
+const pie1 = document.getElementById("pie1");{
+    const h3 = document.createElement("h3");
+    h3.textContent = "Sobre nosotros";
+    const p = document.createElement("p");
+    p.textContent = "Cursos Misión Método de enseñanza Eficacia Equipo Investigación Empleo Guía para uso de marca Tienda Prensa Inversionistas Contáctanos";
+    const h3_1 = document.createElement("h3");
+    h3_1.textContent = "Productos";
+    const p_1 = document.createElement("p");
+    p_1.textContent = "Duolingo Duolingo for Schools Duolingo English Test Podcast Duolingo for Business Súper Duolingo Regala Súper Duolingo";
+    const h3_2 = document.createElement("h3");
+    h3_2.textContent = "Aplicaciones";
+    const p_2 = document.createElement("p");
+    p_2.textContent = "Duolingo para Android Duolingo para iOS";
+    const h3_3 = document.createElement("h3");
+    h3_3.textContent = "Ayuda y soporte";
+    const p_3 = document.createElement("p");
+    p_3.textContent = "App de Duolingo Schools Duolingo English Test Estado";
+    const h3_4 = document.createElement("h3");
+    h3_4.textContent = "Términos y privacidad";
+    const p_4 = document.createElement("p");
+    p_4.textContent = "Normas de la comunidad Términos Privacida";
+    const h3_5 = document.createElement("h3");
+    h3_5.textContent = "Social";
+    const p_5 = document.createElement("p");
+    p_5.textContent = "Blog Instagram Facebook Twitter YouTube";
+}
+const pie2 = document.getElementById("zona12");{
+    const titulo_pie = document.getElementsByClassName("titulo_pie");{
+    const h3 = document.createElement("h3");
+    h3.textContent = "Idioma de la página:";
+    const p = document.createElement("p");
+    p.textContent = "العربية  বাংলা  Čeština  Deutsch  Ελληνικά  English  Español  Français  हिंदी   Magyar  Bahasa Indonesia  Italiano  日本語  한국어  Nederlands  Polski  Português  Română  Русский  తెలుగు  ภาษาไทย  Tagalog  Türkçe  Українською  Tiếng Việt  中文";
+    }
+}
